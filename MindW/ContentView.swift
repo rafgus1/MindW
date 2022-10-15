063e222e8e90cc60e36fe71156ee36918998140d
Rafał Michałowski <rafgus1@wp.pl>
2022-10-15 17:18:28 +0200

    Initial Commit

---
+++ MindW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MindW
+//
+//  Created by Rafał Michałowski on 15/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MindW/MindWApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindWApp.swift
+//  MindW
+//
+//  Created by Rafał Michałowski on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MindWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
